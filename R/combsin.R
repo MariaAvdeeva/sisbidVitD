ed91da19ba26041af9d360c722849cf77761220c
masha57 <mas.avdeeva@gmail.com>
2017-07-18 14:00:10 -0700

    Initial commit

---
+++ R/combsin.R
@@ -0,0 +1,10 @@
+#' The Combined Sine Function
+#'
+#'
+#'
+#' @param x,y
+#' @return sin(x*y)
+#' @examples
+#' combsin(2,5)
+#' @export
+combsin <- function(x,y) {sin(x*y)}
---
+++ R/sisbidVitD.R
@@ -0,0 +1,8 @@
+#'VitD package
+#'
+#'This package analyzes some vitamin D data
+#'
+#'
+#'@docType package
+#'@name sisbidVitD
+NULL
