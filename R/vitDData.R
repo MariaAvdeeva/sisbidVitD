a3c83360b270864da8eedf1314fab7805d7b184a
masha57 <mas.avdeeva@gmail.com>
2017-07-18 22:56:00 -0700

    some documentation changes

---
+++ R/vitDData.R
@@ -0,0 +1,9 @@
+#' Table of OV/BV and serum levels
+#'
+#' These values here summarize the OV/BV ratio of 675 patients and the serum
+#' level provided to them.
+#' @format This table is 675 by 2.\cr
+#' @source ??
+#'
+#' @name vitDData
+NULL
